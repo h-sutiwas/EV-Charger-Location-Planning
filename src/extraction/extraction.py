ef41af76955c871687ed6b2557e0e93fd457321f
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-09-17 14:18:12 +0700

    Update extraction script

--- src/extraction/extraction.py
+++ src/extraction/extraction.py
@@ -1,4 +1,5 @@
 import os
+from collections import deque
 
 import geopy as gp
 import pandas as pd
@@ -9,11 +10,15 @@ import seaborn as sns
 import matplotlib as plt
 
 import openrouteservice as ors
+from shapely.geometry import Point, LineString
 
 import warnings
 warnings.filterwarnings('ignore')
 
 with open(r"api\openrouteservice.pem") as f_in:
     ORS_API = f_in.read()
+
+PROFILE = "driving-car"
+ORS_OPTIONS = {"avoid_borders": "all"}
 
 client = ors.Client(key=ORS_API)
