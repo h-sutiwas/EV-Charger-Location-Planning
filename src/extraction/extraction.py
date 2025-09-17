03b0dc8cfd459c73cda532624a9d9024fb0465bf
Sutiwas Jitsopak <s.jitsopak@gmail.com>
2025-09-17 13:00:59 +0700

    Update this script for extracting the roadshape files from all required town halls

--- src/extraction/extraction.py
+++ src/extraction/extraction.py
@@ -1,11 +1,19 @@
 import os
 
-import geopy
-import pandas
+import geopy as gp
+import pandas as pd
 
-import numpy
-import matplotlib
+import numpy as np
+
+import seaborn as sns
+import matplotlib as plt
+
+import openrouteservice as ors
 
 import warnings
 warnings.filterwarnings('ignore')
 
+with open(r"api\openrouteservice.pem") as f_in:
+    ORS_API = f_in.read()
+
+client = ors.Client(key=ORS_API)
